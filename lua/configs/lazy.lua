18f1b680f22458185e8e93558225a72db7a6152a
Flamur Berisha <flamxi@MacBookPro.fritz.box>
2025-04-06 18:14:13 +0200

    initial update of the configs

--- lua/configs/lazy.lua
+++ lua/configs/lazy.lua
@@ -1,6 +1,6 @@
 return {
   defaults = { lazy = true },
-  install = { colorscheme = { "nvchad" } },
+  install = { colorscheme = { "rosepine" } },
 
   ui = {
     icons = {
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,11 +3,16 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
--- EXAMPLE
-local servers = { "html", "cssls" }
 local nvlsp = require "nvchad.configs.lspconfig"
+local servers = {
+  "html",
+  "clangd",
+  "pyright",
+  "gopls",
+  "rust_analyzer",
+  "lua_ls",
+}
 
--- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = nvlsp.on_attach,
@@ -15,10 +20,3 @@ for _, lsp in ipairs(servers) do
     capabilities = nvlsp.capabilities,
   }
 end
-
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
